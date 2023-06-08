90b7811f62166bdbabc749255ab69bbaa8bbad99
Andriy Kupinskyy <andriy.khk@gmail.com>
2023-06-08 11:17:37 +0200

    make UI for the game RPS v2.0

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -12,7 +12,7 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
-        console.log("no winer");
+        return("no winer");
     }else if(playerSelection == "rock" && computerSelection == "paper"){
         return "computer";
     }else if(playerSelection == "rock" && computerSelection == "scissors"){
@@ -28,39 +28,76 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+let computerScore = 0;
+let playerScore = 0;
 
-function game(){
-    let computerScore = 0;
-    let playerScore = 0;
-    let index = 0;
-
-    while(index < 5){
-        const playerSelection = prompt("Please type rock, paper or scissors:").toLocaleLowerCase(); 
-        const computerSelection = getComputerChoice();
-
-        console.log("\nRound N: " + index + "\n");
-        
-        let winner = playRound(playerSelection, computerSelection);
-        if(winner == "computer"){
-            computerScore++;
-        } 
-        if(winner == "player"){
-            playerScore++;
-        }
-        
-        console.log("Player:" + playerSelection + "\nComputer: " + computerSelection)
-        console.log("Winner of this round is: " + winner);
-        
-        index++;
-    }
-    console.log("Player score: " + playerScore + ", computer score: " + computerScore);
-    if(playerScore > computerScore){
-        console.log("Congratulations! You Won! :)");
-    }else if(playerScore < computerScore){
-        console.log("You lost :(");
-    }else{
-        console.log("No winner, it's a tie! :D");
+function checkWinner(){
+    if(computerScore == 5){
+        const winner = document.querySelector("#winner");
+        winner.textContent = "The winner is Computer";
+    }else if(playerScore == 5){
+        const winner = document.querySelector("#winner");
+        winner.textContent = "The winner is Player";
     }
 }
 
-game();
+const rock = document.getElementById("rockButton");
+const paper = document.getElementById("paperButton");
+const scissors = document.getElementById("scissorsButton");
+
+rock.addEventListener("click", () =>{
+    playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    let winner = playRound(playerSelection, computerSelection);
+    console.log("Player:" + playerSelection + "\nComputer: " + computerSelection + "\nwinner: " + winner);
+
+    if(winner == "computer"){
+        computerScore++;
+    } 
+    if(winner == "player"){
+        playerScore++;
+    }
+
+    const score = document.querySelector("#score");
+    score.textContent = "Computer:" + computerScore + "PLayer: " + playerScore;
+
+    checkWinner();
+})
+    
+paper.addEventListener("click", () =>{
+    playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+    let winner = playRound(playerSelection, computerSelection);
+    console.log("Player:" + playerSelection + "\nComputer: " + computerSelection + "\nwinner: " + winner);
+
+    if(winner == "computer"){
+        computerScore++;
+    } 
+    if(winner == "player"){
+        playerScore++;
+    }
+
+    const score = document.querySelector("#score");
+    score.textContent = "Computer:" + computerScore + "PLayer: " + playerScore;
+
+    checkWinner();
+})
+    
+scissors.addEventListener("click", () =>{
+    playerSelection = "scissors";
+    const computerSelection = getComputerChoice();
+    let winner = playRound(playerSelection, computerSelection);
+    console.log("Player:" + playerSelection + "\nComputer: " + computerSelection + "\nwinner: " + winner);
+
+    if(winner == "computer"){
+        computerScore++;
+    } 
+    if(winner == "player"){
+        playerScore++;
+    }
+
+    const score = document.querySelector("#score");
+    score.textContent = "Computer:" + computerScore + "PLayer: " + playerScore;
+
+    checkWinner();
+})
