099175830adff8cd5d1cc75b36f108ae49386a85
Andriy Kupinskyy <andriy.khk@gmail.com>
2023-06-13 21:07:14 +0200

    create function for operators and numbers in calculator project

--- portfolio/pages/calculator/script.js
+++ portfolio/pages/calculator/script.js
@@ -1,24 +1,49 @@
 let n1 = 3;
 let n2 = 2;
-let operator = "*";
+let operator;
 
 function operate(n1, n2, operator){
     switch(operator){
         case "+":
             return n1 + n2;
-            break;
         case "-":
             return n1 - n2;
-            break;
         case "*":
             return n1 * n2;
-            break;
         case "/":
             return n1 / n2;
-            break;
         default:
             console.log("default");
     }
 }
 
-console.log(operate(n1, n2, operator));
+const display = document.getElementById("display");
+const number = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal").addEventListener("click", operatorEqual);
+const clearButton = document.getElementById("clearButton");
+
+function operatorEqual(e){
+    console.log(e.srcElement.id);
+    let result = operate(n1, n2, operator);
+    display.innerText = result;
+}
+
+operators.forEach((btn) => {
+    btn.addEventListener("click", operatorClicked);
+})
+
+function operatorClicked(e){
+    console.log(e.srcElement.id); //test
+    display.innerText = e.srcElement.id;
+    operator = e.srcElement.id;}
+
+number.forEach((btn) => {
+    btn.addEventListener("click", numberClicked);
+})
+
+function numberClicked(e){
+    console.log(e.srcElement.id); //test
+    display.innerText = e.srcElement.id;
+    n1 = e.srcElement.id;
+}
